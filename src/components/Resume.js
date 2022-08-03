9790a23760c69214edda733724ce8e6de243084c
Moses Manoj <mosesmanoj01@gmail.com>
2022-08-03 17:15:58 +0530

    web resume 3

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,10 +5,10 @@ function Resume(){
                 <div className="text-center fs-1 p-5 w-auto">RESUME</div>
                 <div className="container" style={{width: "fit-content"}}>
                     <div className="row" style={{left:"305px"}}>
-                        <a href="https://drive.google.com/file/d/1giM-cSOtYQDie_iMz67w6VkJAXMWQLlg/view?usp=sharing" target="_blank" rel="noreferrer"><img src = "/data/img/download.png" alt="download-png" /></a>
+                        <a href="https://drive.google.com/file/d/1giM-cSOtYQDie_iMz67w6VkJAXMWQLlg/view?usp=sharing" target="_blank" rel="noreferrer"><img src = "https://manojkonda01.github.io/webresume/data/img/download.png" alt="download-png" /></a>
                     </div>
                     <div className="row text-center pb-5 " >
-                        <img src="/data/img/resume.jpg" alt="manoj-konda-resume" height={1200}/>
+                        <img src="https://manojkonda01.github.io/webresume/data/img/resume.jpg" alt="manoj-konda-resume" height={1200}/>
                     </div>
                 </div>
                 
