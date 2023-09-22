098f801938241bbb20f18bdd78be28f9b11ba5cb
mkonda1-admin <mosesmanoj01@gmail.com>
2023-09-21 19:55:19 -0500

    emle

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,8 +1,8 @@
-import ContactDetails from "./ContactDetails";
-import VerticalLineContent from "./VerticalLine/VerticalLineContent";
-import VerticalLineWrapper from "./VerticalLine/VerticalLineWrapper";
-import ProgressBar from "./Skills";
-import { useState, useEffect } from "react";
+import ContactDetails from './ContactDetails';
+import VerticalLineContent from './VerticalLine/VerticalLineContent';
+import VerticalLineWrapper from './VerticalLine/VerticalLineWrapper';
+import ProgressBar from './Skills';
+import { useState, useEffect } from 'react';
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
@@ -20,62 +20,62 @@ function Details() {
       setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
   const style = {
-    personal_card: { width: "60%" },
-    image_dimensions: { width: "20rem", height: "20rem" },
-    bg_className: "d-flex justify-content-center align-items-center p-5",
-    textAlign: "justify",
-    fontSize: { heading: "60px", paragraph: "20px" },
-    leftAlign: "right",
-    skills:{className:"p-5 text-white"}
+    personal_card: { width: '60%' },
+    image_dimensions: { width: '20rem', height: '20rem' },
+    bg_className: 'd-flex justify-content-center align-items-center p-5',
+    textAlign: 'justify',
+    fontSize: { heading: '60px', paragraph: '20px' },
+    leftAlign: 'right',
+    skills: { className: 'p-5 text-white' },
   };
   if (windowDimensions.width <= 800) {
-    style.personal_card.width = "100%";
-    style.image_dimensions.width = "15rem";
-    style.image_dimensions.height = "15rem";
-    style.bg_className = "d-flex justify-content-center align-items-center p-4";
-    style.textAlign = "left";
-    style.fontSize = { heading: "40px", paragraph: "18px" };
-    style.leftAlign = "left";
-    style.skills.className = "p-2 text-white"
+    style.personal_card.width = '100%';
+    style.image_dimensions.width = '15rem';
+    style.image_dimensions.height = '15rem';
+    style.bg_className = 'd-flex justify-content-center align-items-center p-4';
+    style.textAlign = 'left';
+    style.fontSize = { heading: '40px', paragraph: '18px' };
+    style.leftAlign = 'left';
+    style.skills.className = 'p-2 text-white';
   }
 
-  const [hoverColor, setHoverColor] = useState({ color: "white" });
-   const handleMouseEnter = () => {
-      setHoverColor({color : "blue"});
-   };
-   const handleMouseLeave = () => {
-      setHoverColor({color : "white"});
-   };
+  const [hoverColor, setHoverColor] = useState({ color: 'white' });
+  const handleMouseEnter = () => {
+    setHoverColor({ color: 'blue' });
+  };
+  const handleMouseLeave = () => {
+    setHoverColor({ color: 'white' });
+  };
   return (
     <>
       <div className="h-auto w-auto m-0">
         <div
           style={{
-            fontFamily: "Saira",
+            fontFamily: 'Saira',
             textAlign: style.textAlign,
-            overflowY: "auto",
+            overflowY: 'auto',
           }}
         >
           <div
             className={style.bg_className}
             style={{
-              height: "auto",
+              height: 'auto',
               backgroundImage: `url("https://manojkonda01.github.io/webresume/data/img/main_bg.png")`,
-              backgroundPosition: "bottom",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-              display: "block",
+              backgroundPosition: 'bottom',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              display: 'block',
             }}
           >
             <div
               className="shadow container m-0"
               id="personal-card"
               style={{
-                backgroundColor: "white",
+                backgroundColor: 'white',
                 width: style.personal_card.width,
               }}
             >
@@ -88,21 +88,21 @@ function Details() {
                     style={{
                       width: style.image_dimensions.width,
                       height: style.image_dimensions.height,
-                      float: "left",
-                      objectFit: "cover",
+                      float: 'left',
+                      objectFit: 'cover',
                     }}
                   />
                 </div>
                 <div className="col">
                   <h1
                     className=" p-3 pb-0 text-center"
-                    style={{ fontSize: "80px" }}
+                    style={{ fontSize: '80px' }}
                   >
                     Manoj<br></br>Konda
                   </h1>
                   <h6
                     className="p-0 text-center"
-                    style={{ fontFamily: "Saira" }}
+                    style={{ fontFamily: 'Saira' }}
                   >
                     Software Engineer
                   </h6>
@@ -120,76 +120,81 @@ function Details() {
                 className="p-5 bg-white text-white"
                 style={{
                   backgroundImage: `url("https://manojkonda01.github.io/webresume/data/img/skills.png")`,
-                  backgroundPosition: "bottom",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  display: "block",
+                  backgroundPosition: 'bottom',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  display: 'block',
                 }}
               >
                 <div
                   className="container text-centre"
-                  style={{ display: "flex", flexWrap: "wrap" }}
+                  style={{ display: 'flex', flexWrap: 'wrap' }}
                 >
                   <div
                     className="row d-flex justify-content-center align-items-center"
-                    style={{ fontWeight: "bold" }}
+                    style={{ fontWeight: 'bold' }}
                   >
                     <div id="about" className="">
                       <h1 style={{ fontSize: style.fontSize.heading }}>
-                        {" "}
-                        About{" "}
+                        {' '}
+                        About{' '}
                       </h1>
-                      <hr style={{ height: "5px", background: "white" }} />
+                      <hr style={{ height: '5px', background: 'white' }} />
                       <p style={{ fontSize: style.fontSize.paragraph }}>
-                        Software Engineer, with a passion for developing
-                        innovative programs that expedite the efficiency seeking
-                        to pursue a successfull career in a company where my
-                        skills can be most effectively utilized. Quick learner,
-                        Strategic thinker, and innovative creator to develop
-                        software that is customized to meet a company’s
-                        organizational needs. I can contribute towards the goals
-                        of your company, building my career along the way.
+                        Dedicated programmer with a background in web
+                        development and design, aiming to contribute to the
+                        school by effectively managing website updates,
+                        designing engaging web pages. Committed to delivering
+                        exceptional results adhering to deadlines and
+                        collaborating seamlessly.
                       </p>
                     </div>
 
                     <div id="experience" className="pt-5">
                       <h1 style={{ fontSize: style.fontSize.heading }}>
-                        {" "}
-                        Experience{" "}
+                        {' '}
+                        Experience{' '}
                       </h1>
-                      <hr style={{ height: "5px", background: "white" }} />
+                      <hr style={{ height: '5px', background: 'white' }} />
 
                       <div style={{ fontSize: style.fontSize.paragraph }}>
                         <VerticalLineWrapper color="red">
                           <VerticalLineContent position="left">
-                            <strong>2021 Oct - present</strong>
-                            <h5>POKKT</h5>
+                            <strong>2021 Oct - Aug 2023</strong>
+                            <a
+                              href="https://drive.google.com/file/d/1qNRKCcK2oGX8C08oy2N_eZC4eNfF8Je_/view?usp=sharing"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <h5>POKKT</h5>
+                            </a>
+
                             <p style={{ textAlign: style.leftAlign }}>
-                              Currently Working as Software Engineer for the
-                              product at POKKT.<br></br>
+                              Worked as Software Engineer for the product at
+                              POKKT.<br></br>
                             </p>
                             <p
                               style={{
-                                textAlign: "right",
-                                fontWeight: "lighter",
+                                textAlign: 'right',
+                                fontWeight: 'lighter',
                               }}
                             >
                               <span
-                                style={{ fontWeight: "bold", fontStyle: "un" }}
+                                style={{ fontWeight: 'bold', fontStyle: 'un' }}
                               >
                                 <u>Back- End:</u>
-                              </span>{" "}
-                              PHP | CodeIgniter 3 {"&"} 4 ||||| Ktor<br></br>
-                              <span style={{ fontWeight: "bold" }}>
+                              </span>{' '}
+                              PHP | CodeIgniter 3 {'&'} 4 ||||| Ktor<br></br>
+                              <span style={{ fontWeight: 'bold' }}>
                                 <u>Front- End:</u>
-                              </span>{" "}
+                              </span>{' '}
                               HTML, CSS, JS, JQuery, React JS
                             </p>
                           </VerticalLineContent>
                           <VerticalLineContent position="right">
                             <strong>26 May 2021 - 9 July 2021</strong>
                             <a
-                              href="https://drive.google.com/file/d/1OV6rSa5DZIMm7DVCDS7o8ZlG26Zdewzj/view?usp=sharing"
+                              href="https://drive.google.com/file/d/1Co51dQElBQFAs31Q5Cn0vV06jwSNx3Hx/view?usp=sharing"
                               target="_blank"
                               rel="noreferrer"
                             >
@@ -212,18 +217,18 @@ function Details() {
                 className="p-5 bg-white text-white"
                 style={{
                   backgroundImage: `url("https://manojkonda01.github.io/webresume/data/img/education.png")`,
-                  backgroundPosition: "bottom",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  display: "block",
+                  backgroundPosition: 'bottom',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  display: 'block',
                 }}
               >
                 <div id="education" className="p-5">
                   <h1 style={{ fontSize: style.fontSize.heading }}>
-                    {" "}
-                    Education{" "}
+                    {' '}
+                    Education{' '}
                   </h1>
-                  <hr style={{ height: "5px", background: "white" }} />
+                  <hr style={{ height: '5px', background: 'white' }} />
                   <div style={{ fontSize: style.fontSize.paragraph }}>
                     <VerticalLineWrapper color="red">
                       <VerticalLineContent position="left">
@@ -263,15 +268,15 @@ function Details() {
                 className={style.skills.className}
                 style={{
                   backgroundImage: `url("https://manojkonda01.github.io/webresume/data/img/profile.png")`,
-                  backgroundPosition: "bottom",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                  display: "block",
+                  backgroundPosition: 'bottom',
+                  backgroundSize: 'cover',
+                  backgroundRepeat: 'no-repeat',
+                  display: 'block',
                 }}
               >
                 <h1 style={{ fontSize: style.fontSize.heading }}> Skills </h1>
 
-                <hr style={{ height: "5px", background: "white" }} />
+                <hr style={{ height: '5px', background: 'white' }} />
                 <div
                   style={{ fontSize: style.fontSize.paragraph }}
                   className="container p-3 w-75"
@@ -295,7 +300,18 @@ function Details() {
                   <ProgressBar name="KTOR" value="75" color="#fd0d85" />
                   <ProgressBar
                     name="MySQl, MongoDB, "
-                    component = {<a href='https://drive.google.com/file/d/1cwpC5GFjd2tzOJqsPuf9hUhOWbI1bQLJ/view?usp=sharing' target="_blank" rel="noreferrer" style={hoverColor}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>Google BigQuery</a>}
+                    component={
+                      <a
+                        href="https://drive.google.com/file/d/1cwpC5GFjd2tzOJqsPuf9hUhOWbI1bQLJ/view?usp=sharing"
+                        target="_blank"
+                        rel="noreferrer"
+                        style={hoverColor}
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                      >
+                        Google BigQuery
+                      </a>
+                    }
                     value="80"
                     color="#4805f4"
                   />
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,20 +1,32 @@
-function Resume(){
-    return(
-        <>
-            <div className="w-auto h-auto" style={{backgroundColor: "#e6e6ff"}}>
-                <div className="text-center fs-1 p-5 w-auto">RESUME</div>
-                <div className="container" style={{width: "fit-content"}}>
-                    <div className="row" style={{left:"305px"}}>
-                        <a href="https://drive.google.com/file/d/1giM-cSOtYQDie_iMz67w6VkJAXMWQLlg/view?usp=sharing" target="_blank" rel="noreferrer"><img src = "https://manojkonda01.github.io/webresume/data/img/download.png" alt="download-png" /></a>
-                    </div>
-                    <div className="row text-center pb-5 " >
-                        <img src="https://manojkonda01.github.io/webresume/data/img/resume.jpg" alt="manoj-konda-resume" height={1200}/>
-                    </div>
-                </div>
-                
-            </div>
-        </>
-    )
+function Resume() {
+  return (
+    <>
+      <div className="w-auto h-auto" style={{ backgroundColor: '#e6e6ff' }}>
+        <div className="text-center fs-1 p-5 w-auto">RESUME</div>
+        <div className="container" style={{ width: 'fit-content' }}>
+          <div className="row" style={{ left: '305px' }}>
+            <a
+              href="https://drive.google.com/file/d/1I-kjeTFrE73AXoA3SJn1T74UwRPsKR9Z/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://manojkonda01.github.io/webresume/data/img/download.png"
+                alt="download-png"
+              />
+            </a>
+          </div>
+          <div className="row text-center pb-5 ">
+            <img
+              src="https://manojkonda01.github.io/webresume/data/img/resume.jpg"
+              alt="manoj-konda-resume"
+              height={1200}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Resume;
